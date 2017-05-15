af6e07f8e1edffbe801145b545beab3120136b0e
mucun1988 <mucun1988@gmail.com>
2017-05-14 22:31:32 -0400

    testtt

--- R/routine.R
+++ R/routine.R
@@ -1,7 +1,2 @@
-#' Concatenate two strings
-#'
-#' @param a: string in front
-#' @param b: string in back
-#' @return concatenate string
 #' @export
-`%+%` <- function(a, b)  paste0(a, b)
+square <- function(x)  return(x^2)
