5bd8cc77402d20cb90f4e647ae771be5def1c88f
mucun1988 <mucun1988@gmail.com>
2017-05-14 21:44:36 -0400

    add %+%
    add %+%

---
+++ R/routine.R
@@ -0,0 +1,10 @@
+#' Square a number
+#'
+#' concatenate two strings
+#' @param a: string in front
+#' @param b: string in back
+#' @return concatenate string
+#' @export
+'%+%' <- function(a, b){
+  paste0(a, b)
+}
