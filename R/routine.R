58b256fc85ebfb4df274430759a95b8c77397bcb
mucun1988 <mucun1988@gmail.com>
2017-05-29 20:45:29 -0400

    square

--- R/routine.R
+++ R/routine.R
@@ -1,2 +1,9 @@
+#' Square a number
+#'
+#' Take in any numeric value and square it
+#' @param x A numeric value to be squared
+#' @return The square of the input
 #' @export
-square <- function(x)  return(x^2)
+square <- function(x){
+  return(x^2)
+}
